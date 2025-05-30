f8588842a8d719343c72906bec49787ebcc1760d
Kine16T <kine16t@gmail.com>
2025-05-29 20:49:12 -1200

    carousel

--- Amazon_Client/src/App.jsx
+++ Amazon_Client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Components/Header/Header'
+import CarouselEffect from './Components/Carousel/CourselEffect'
 
 function App() {
  return (
     <div>
      <Header/>
+     <CarouselEffect/>
     </div>
   )
 }
---
+++ Amazon_Client/src/Components/Carousel/img/data.js
@@ -0,0 +1,8 @@
+import img1 from "./beutyproduct.jpg";
+import img2 from "./gaming.jpg";
+import img3 from "./shopbook.jpg";
+import img4 from "./new arrival.jpg";
+import img5 from "./kitchen.jpg";
+
+const img = [img1, img2, img3, img4, img5];
+export default img;
